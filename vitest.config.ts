7d593df2ec0c237fa27f74070580f43cdc4276dd
Ewanderson Flaviano <inovaprog@gmail.com>
2026-09-24 10:27:50 -0300

    Estabiliza testes que compartilham armazenamento

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+    fileParallelism: false,
     setupFiles: ["./test/setup.ts"],
     clearMocks: true,
     coverage: {
